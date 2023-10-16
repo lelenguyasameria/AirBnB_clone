0341b6ad48010000ab6e7c85e47f4f03e98b38c5
lelenguyasameria <somearia711@gmail.com>
2023-10-16 01:08:31 -0700

    intClone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,13 @@ Serializes instances to a JSON file and deserializes JSON file to instances
 """
 import json
 import models
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """Serializes instances to a JSON file and deserializes
