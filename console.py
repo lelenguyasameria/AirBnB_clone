e885859af0dd9c976ce098ff2f0736e21cae02a0
lelenguyasameria <somearia@gmail.com>
2023-10-16 10:46:04 +0000

    intClone

--- console.py
+++ console.py
@@ -4,12 +4,23 @@ Contains the HBNBCommand class for the command interpreter.
 """
 
 import cmd
-
+import re
+from models.base_model import BaseModel
+from shlex import split
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for the Holberton Airbnb Clone project."""
 
     prompt = "(hbnb) "
+
+    valid_classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -24,6 +35,103 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing when an empty line is entered"""
         pass
 
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, save it, and print the id."""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        if key in storage.all():
+            print(storage.all()[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on class name and id (save the change into the JSON file)."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        if key in storage.all():
+            del storage.all()[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representations of instances based on class name or all classes."""
+        args = arg.split()
+        obj_list = []
+        if not args:
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+            print(obj_list)
+            return
+        if args[0] not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+        for key, obj in storage.all().items():
+            if args[0] in key:
+                obj_list.append(str(obj))
+        print(obj_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on class name and id by adding or updating an attribute."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attr_name = args[2]
+        attr_value = args[3]
+        try:
+            obj = storage.all()[key]
+            setattr(obj, attr_name, eval(attr_value))
+            obj.save()
+        except AttributeError:
+            print("** no instance found **")
+        except Exception:
+            pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
